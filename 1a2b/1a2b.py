395ef2da0ff2404327161b2114d3f3a4969c6acc
wuling31715 <wuling31715@gmail.com>
2017-09-20 17:45:30 +0800

    first commit

---
+++ 1a2b/1a2b.py
@@ -0,0 +1,56 @@
+import random 
+
+def main():
+    global final_answer, game_continue, round_times
+    final_answer = answer_create()
+    #print(final_answer)
+    game_continue = True 
+    round_times = 1
+
+    while game_continue:
+        guess_input = input('Please input a 4 digits number:\n')
+        if answer_check(guess_input):
+            pass
+        else:
+            hint = get_hint(guess_input)
+            A = hint[0]
+            B = hint[1]
+            print('%d A %d B' % (A, B))
+        round_times = round_times + 1
+
+def answer_create():
+    number = [i for i in range(10)]
+    random_number = random.sample(number, 4)
+    answer = ''
+    for i in random_number:
+        answer = answer + str(i)
+    return answer    
+
+def answer_check(guess_input):
+    global game_continue, round_times 
+    if final_answer == guess_input:
+        game_continue = False
+        print('4 A 0 B\nRound Times: %d' %(round_times))
+        return True
+    else:
+        return False
+
+def get_hint(guess_input):
+    global final_answer
+    A = 0
+    B = 0
+    final_answer_tmp = ''
+    guess_input_tmp = ''
+    for i, j in zip (final_answer, guess_input):
+        if i == j:
+            A = A + 1
+        else:
+            final_answer_tmp = final_answer_tmp + i
+            guess_input_tmp = guess_input_tmp + j
+    for i in final_answer_tmp:
+        for j in guess_input_tmp:
+            if i == j:
+                B = B + 1
+    return A, B            
+
+main()
