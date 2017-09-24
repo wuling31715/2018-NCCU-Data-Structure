34e6acc3cd003910707d4143726dc843d21463cf
wuling31715 <wuling31715@gmail.com>
2017-09-24 21:22:01 +0800

    upload

--- 1a2b/1a2b.py
+++ 1a2b/1a2b.py
@@ -55,4 +55,4 @@ time_start = time.time()
 main()
 time_end = time.time()
 time_used = time_end - time_start
-print('Time Used: %d (s)' % (round(time_used)))
+print('Time Used: %d (S)' % (round(time_used)))
